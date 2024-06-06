206e1ec0e29d64df1f6e07469ddeab3fa2d27697
banepanist <nistbanepa4@gmail.com>
2024-06-06 08:18:00 +0545

    Add files via upload

---
+++ stringmethod.js
@@ -0,0 +1,25 @@
+// var a=3;
+// alert(a);
+var text="i am currently involved in BSC.CSIT 5th semester."
+// alert(text);
+console.log(text)
+
+// print the length of the string
+console.log("The length of the text is:",text.length)
+
+// print the character at index 5 of the given string
+console.log("The character at index 5 using charAt() is:",text.charAt(5));
+
+// get the 6th letter of name
+console.log("The character at index 5  is:",text[5]);
+// print the code of the character at a  index 5 
+console.log("the code of the character at a specific index",text.charCodeAt(5));
+
+// print and write it into the document,the text between 3 and 10 index
+console.log("The text between 3 and 10 index is:",text.slice(3,40));
+
+// write it in the html document
+document.write("The text between 3 and 10 index is:")
+
+// change the first letter to the upperCase
+console.log(text.toUpperCase());
