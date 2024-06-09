3d3cb00a04038ed108e92151b67d27060e435554
banepanist <nistbanepa4@gmail.com>
2024-06-09 08:17:08 +0545

    Add files via upload

---
+++ numberMethod.js
@@ -0,0 +1,15 @@
+var xyz=123456;
+// convert number to string
+console.log("Converting number to the string",xyz.toString());
+
+// returns a string, with a number rounded 
+let y=2;
+z=y.toExponential(3);
+console.log(z);
+
+
+let a = 9.656;
+console.log(" returns a string, with the number written with a specified number of decimals",a.toFixed(0));
+// a.toFixed(2);
+// a.toFixed(4);
+console.log(" returns a string, with the number written with a specified number of decimals",a.toFixed(6));
