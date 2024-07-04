92c712ccc8f5bfd20ffd10182335ef7321825818
banepanist <nistbanepa4@gmail.com>
2024-07-04 07:28:26 +0545

    Add files via upload

---
+++ toggle.js
@@ -0,0 +1,8 @@
+function change(){
+    const target =document.getElementById("random");
+    if(target.style.backgroundColor =="pink")
+        target.style.backgroundColor ="blue";
+    else{
+        target.style.backgroundColor ="pink"
+    }
+}
